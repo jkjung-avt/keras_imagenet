b3777a56b1b05a8ef1822fb87f29d1e077afc84c
JK Jung <jkjung13@gmail.com>
2019-06-16 19:13:12 +0800

    Refactor get_mobilenet_v2()

--- models/models.py
+++ models/models.py
@@ -17,20 +17,27 @@ INITIAL_LR = 4e-4    # Initial learning rate
 LR_DECAY = 1e-6      # Learning rate decay
 
 
+def get_mobilenet_v2():
+    """Build mobilenet_v2 training model."""
+    model = tf.keras.applications.mobilenet_v2.MobileNetV2(
+        include_top=True,
+        weights=None,
+        classes=1000)
+    # TO-DO: add weight decay here
+    optimizer = convert_to_accum_optimizer(
+        tf.keras.optimizers.Adam(lr=INITIAL_LR, decay=LR_DECAY),
+        config.ITER_SIZE)
+    model.compile(
+        optimizer=optimizer,
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
+    return model
+
+
 def get_training_model(model_name):
     """Build the model to be trained."""
     if model_name == 'mobilenet_v2':
-        model = tf.keras.applications.mobilenet_v2.MobileNetV2(
-            include_top=True,
-            weights=None,
-            classes=1000)
-        optimizer = convert_to_accum_optimizer(
-            tf.keras.optimizers.Adam(lr=INITIAL_LR, decay=LR_DECAY),
-            config.ITER_SIZE)
-        model.compile(
-            optimizer=optimizer,
-            loss='categorical_crossentropy',
-            metrics=['accuracy'])
+        model = get_mobilenet_v2()
     else:
         raise ValueError
 

aaccc5265eca9fd709ed253d2a9f0fa8d5b22b11
JK Jung <jkjung13@gmail.com>
2019-06-18 09:35:25 +0800

    Add 'nasnet_mobile' model

--- models/models.py
+++ models/models.py
@@ -12,29 +12,9 @@ from config import config
 from utils.optimizer import convert_to_accum_optimizer
 
 
-def get_mobilenet_v2(initial_lr, lr_decay, iter_size):
-    """Build MobileNetV2 training model."""
-    model = tf.keras.applications.mobilenet_v2.MobileNetV2(
-        include_top=True,
-        weights=None,
-        classes=1000)
-    # TO-DO: add weight decay here
-    optimizer = convert_to_accum_optimizer(
-        tf.keras.optimizers.Adam(lr=initial_lr, decay=lr_decay),
-        iter_size)
-    model.compile(
-        optimizer=optimizer,
-        loss='categorical_crossentropy',
-        metrics=['accuracy'])
-    return model
-
-
-def get_resnet50(initial_lr, lr_decay, iter_size):
-    """Build NasNetMobile training model."""
-    model = tf.keras.applications.resnet50.ResNet50(
-        include_top=True,
-        weights=None,
-        classes=1000)
+def _get_model(model_func, initial_lr, lr_decay, iter_size):
+    """Build keras model."""
+    model = model_func(include_top=True, weights=None, classes=1000)
     # TO-DO: add weight decay here
     optimizer = convert_to_accum_optimizer(
         tf.keras.optimizers.Adam(lr=initial_lr, decay=lr_decay),
@@ -51,6 +31,8 @@ def get_batch_size(model_name, value):
         return value
     elif model_name == 'mobilenet_v2':
         return 64
+    elif model_name == 'nasnet_mobile':
+        return 32
     elif model_name == 'resnet50':
         return 16
     else:
@@ -62,6 +44,8 @@ def get_iter_size(model_name, value):
         return value
     elif model_name == 'mobilenet_v2':
         return 4
+    elif model_name == 'nasnet_mobile':
+        return 8
     elif model_name == 'resnet50':
         return 16
     else:
@@ -79,9 +63,14 @@ def get_lr_decay(model_name, value):
 def get_training_model(model_name, iter_size, initial_lr, lr_decay):
     """Build the model to be trained."""
     if model_name == 'mobilenet_v2':
-        model = get_mobilenet_v2(initial_lr, lr_decay, iter_size)
+        model = _get_model(tf.keras.applications.mobilenet_v2.MobileNetV2,
+                           initial_lr, lr_decay, iter_size)
+    elif model_name == 'nasnet_mobile':
+        model = _get_model(tf.keras.applications.nasnet.NASNetMobile,
+                           initial_lr, lr_decay, iter_size)
     elif model_name == 'resnet50':
-        model = get_resnet50(initial_lr, lr_decay, iter_size)
+        model = _get_model(tf.keras.applications.resnet50.ResNet50,
+                           initial_lr, lr_decay, iter_size)
     else:
         raise ValueError
     print(model.summary())

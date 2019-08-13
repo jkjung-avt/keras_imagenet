590f8635ebde13677f60f62c6cce0ee9b5f11f9a
JK Jung <jkjung13@gmail.com>
2019-08-13 09:04:04 +0800

    Bump up the 'weight_decay' in train_resnet50.sh

--- train_resnet50.sh
+++ train_resnet50.sh
@@ -4,18 +4,18 @@ set -xe
 # Train resnet50 ILSVRC2012 model in 4 stages.
 # Total 20 + 40 + 60 + 80 = 200 epochs.
 
-python3 train.py --batch_size 16 --iter_size 16 --initial_lr 3e-4 --final_lr 1e-6 --weight_decay 0.0025 --epochs 20 resnet50
+python3 train.py --batch_size 16 --iter_size 16 --initial_lr 4e-4 --final_lr 1e-6 --weight_decay 0.1 --epochs 20 resnet50
 
 cp saves/resnet50-model-final.h5 saves/resnet50-stage1.h5
 
-python3 train.py --batch_size 16 --iter_size 16 --initial_lr 3e-4 --final_lr 1e-6 --weight_decay 0.0025 --epochs 40 saves/resnet50-stage1.h5
+python3 train.py --batch_size 16 --iter_size 16 --initial_lr 2e-4 --final_lr 1e-6 --weight_decay 0.1 --epochs 40 saves/resnet50-stage1.h5
 
 cp saves/resnet50-model-final.h5 saves/resnet50-stage2.h5
 
-python3 train.py --batch_size 16 --iter_size 16 --initial_lr 3e-4 --final_lr 1e-6 --weight_decay 0.0025 --epochs 60 saves/resnet50-stage2.h5
+python3 train.py --batch_size 16 --iter_size 16 --initial_lr 1e-4 --final_lr 1e-6 --weight_decay 0.1 --epochs 60 saves/resnet50-stage2.h5
 
 cp saves/resnet50-model-final.h5 saves/resnet50-stage3.h5
 
-python3 train.py --batch_size 16 --iter_size 16 --initial_lr 3e-4 --final_lr 1e-6 --weight_decay 0.0025 --epochs 80 saves/resnet50-stage3.h5
+python3 train.py --batch_size 16 --iter_size 16 --initial_lr 5e-5 --final_lr 1e-6 --weight_decay 0.1 --epochs 80 saves/resnet50-stage3.h5
 
 cp saves/resnet50-model-final.h5 saves/resnet50-stage4.h5

cdfb65f91e934bdfe718852c800f2f467b483639
JK Jung <jkjung13@gmail.com>
2019-10-20 19:56:48 +0800

    Add '--optimizer' as an option for train.py; simplify weight_decay related code

--- models/models.py
+++ models/models.py
@@ -14,6 +14,10 @@ from models.adamw import AdamW
 from models.optimizer import convert_to_accum_optimizer
 
 
+IN_SHAPE = (224, 224, 3)  # shape of input image tensor
+NUM_CLASSES = 1000        # number of output classes (1000 for ImageNet)
+
+
 def _set_l2(model, weight_decay):
     """Add L2 regularization into layers with weights
 
@@ -61,59 +65,73 @@ def get_iter_size(model_name, value):
 
 
 def get_initial_lr(model_name, value):
-    return value if value > 0 else 3e-4
+    return value if value > 0. else 1e-1
 
 
 def get_final_lr(model_name, value):
-    return value if value > 0 else 1e-5
+    return value if value > 0. else 1e-5
 
 
 def get_weight_decay(model_name, value):
-    return value if value > 0 else 1e-5
+    return value if value >= 0. else 1e-5
 
 
-def get_training_model(model_name, iter_size, initial_lr, weight_decay,
-                       use_weight_decay=True, use_l2_regularization=False,
-                       weight_decay_normalizer=1.):
-    if use_weight_decay + use_l2_regularization >= 2:
+def get_optimizer(model_name, optim_name, initial_lr, epsilon=1e-2):
+    """get_optimizer
+
+    Note that learning rate decay is implemented as a callback in
+    the model.fit(), so I do not specify 'decay' in the optimizers
+    here.
+    """
+    if optim_name == 'sgd':
+        return tf.keras.optimizers.SGD(lr=initial_lr, momentum=0.9)
+    elif optim_name == 'adam':
+        return tf.keras.optimizers.Adam(lr=initial_lr, epsilon=epsilon)
+    else:
+        # implementation of 'AdamW' is removed temporarily
         raise ValueError
+
+
+def get_training_model(model_name, optimizer, iter_size, weight_decay):
     """Build the model to be trained."""
     if model_name.endswith('.h5'):
+        # load a saved model
         model = tf.keras.models.load_model(
             model_name,
             custom_objects={'AdamW': AdamW})
     else:
+        # initialize the model from scratch
         if model_name == 'mobilenet_v2':
-            model = tf.keras.applications.mobilenet_v2.MobileNetV2(
-                input_shape=(224,224,3), include_top=False, weights=None)
+            backbone = tf.keras.applications.mobilenet_v2.MobileNetV2(
+                input_shape=IN_SHAPE, include_top=False, weights=None)
         elif model_name == 'nasnet_mobile':
-            model = tf.keras.applications.nasnet.NASNetMobile(
-                input_shape=(224,224,3), include_top=False, weights=None)
+            backbone = tf.keras.applications.nasnet.NASNetMobile(
+                input_shape=IN_SHAPE, include_top=False, weights=None)
         elif model_name == 'resnet50':
-            model = tf.keras.applications.resnet50.ResNet50(
-                input_shape=(224,224,3), include_top=False, weights=None)
+            backbone = tf.keras.applications.resnet50.ResNet50(
+                input_shape=IN_SHAPE, include_top=False, weights=None)
         else:
             raise ValueError
-        x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+        # Add a Dropout layer before the final Dense output
+        x = tf.keras.layers.GlobalAveragePooling2D()(backbone.output)
         x = tf.keras.layers.Dropout(0.5)(x)
-        x = tf.keras.layers.Dense(1000, activation='softmax', name='Logits')(x)
-        model = tf.keras.models.Model(inputs=model.input, outputs=x)
+        x = tf.keras.layers.Dense(
+            NUM_CLASSES, activation='softmax', name='Logits')(x)
+        model = tf.keras.models.Model(inputs=backbone.input, outputs=x)
 
-    if use_weight_decay:
-        optimizer = AdamW(lr=initial_lr,
-                          weight_decay=weight_decay,
-                          weight_decay_normalizer=weight_decay_normalizer)
-    else:
-        if use_l2_regularization:
-            if (weight_decay > 0):
-                _set_l2(model, weight_decay)
-        optimizer = tf.keras.optimizers.Adam(lr=initial_lr)
+    if weight_decay > 0.:
+        _set_l2(model, weight_decay)
     if iter_size > 1:
         optimizer = convert_to_accum_optimizer(optimizer, iter_size)
+
+    # make sure all layers are set to be trainable
+    for layer in model.layers:
+        layer.trainable = True
 
     model.compile(
         optimizer=optimizer,
         loss='categorical_crossentropy',
         metrics=['accuracy'])
     print(model.summary())
+
     return model
--- train.py
+++ train.py
@@ -12,19 +12,24 @@ import tensorflow as tf
 
 from config import config
 from utils.dataset import get_dataset
-from models.models import get_batch_size, get_iter_size
-from models.models import get_initial_lr, get_final_lr
-from models.models import get_weight_decay, get_training_model
+from models.models import get_batch_size
+from models.models import get_iter_size
+from models.models import get_initial_lr
+from models.models import get_final_lr
+from models.models import get_weight_decay
+from models.models import get_optimizer
+from models.models import get_training_model
 
 
 DESCRIPTION = """For example:
 $ python3 train.py --dataset_dir  ${HOME}/data/ILSVRC2012/tfrecords \
+                   --optimizer    adam \
                    --batch_size   64 \
                    --iter_size    4 \
-                   --initial_lr   3e-4 \
+                   --initial_lr   1e-1 \
                    --final_lr     1e-5 \
-                   --weight_decay 1e-5 \
-                   --epochs       50 \
+                   --weight_decay 0.0 \
+                   --epochs       60 \
                    mobilenet_v2
 """
 SUPPORTED_MODELS = (
@@ -36,7 +41,6 @@ def config_keras_backend():
     """Config tensorflow backend to use less GPU memory."""
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    #config.gpu_options.per_process_gpu_memory_fraction = 0.5
     session = tf.Session(config=config)
     tf.keras.backend.set_session(session)
 
@@ -52,18 +56,19 @@ def get_lrate_func(initial_lr, final_lr, total_epochs):
     return step_decay
 
 
-def train(model_name, batch_size, iter_size, initial_lr, final_lr,
-          weight_decay, epochs, dataset_dir):
+def train(model_name, optim_name, batch_size, iter_size,
+          initial_lr, final_lr, weight_decay, epochs, dataset_dir):
     """Prepare data and train the model."""
-    batch_size = get_batch_size(model_name, batch_size)
-    iter_size = get_iter_size(model_name, iter_size)
-    initial_lr = get_initial_lr(model_name, initial_lr)
-    final_lr = get_final_lr(model_name, final_lr)
+    batch_size   = get_batch_size(model_name, batch_size)
+    iter_size    = get_iter_size(model_name, iter_size)
+    initial_lr   = get_initial_lr(model_name, initial_lr)
+    final_lr     = get_final_lr(model_name, final_lr)
+    optimizer    = get_optimizer(model_name, optim_name, initial_lr)
     weight_decay = get_weight_decay(model_name, weight_decay)
 
     # get training and validation data
     ds_train = get_dataset(dataset_dir, 'train', batch_size)
-    ds_validation = get_dataset(dataset_dir, 'validation', batch_size)
+    ds_valid = get_dataset(dataset_dir, 'validation', batch_size)
 
     # instantiate training callbacks
     lrate = tf.keras.callbacks.LearningRateScheduler(
@@ -71,27 +76,25 @@ def train(model_name, batch_size, iter_size, initial_lr, final_lr,
     save_name = model_name if not model_name.endswith('.h5') else \
                 os.path.split(model_name)[-1].split('.')[0].split('-')[0]
     model_ckpt = tf.keras.callbacks.ModelCheckpoint(
-        '{}/{}'.format(config.SAVE_DIR, save_name) + '-ckpt-{epoch:03d}.h5',
+        os.path.join(config.SAVE_DIR, save_name) + '-ckpt-{epoch:03d}.h5',
         monitor='val_loss',
         save_best_only=True)
     tensorboard = tf.keras.callbacks.TensorBoard(
         log_dir='{}/{}'.format(config.LOG_DIR, time.time()))
 
     # build model and do training
-    weight_decay_normalizer = 1. / (((1281167*epochs) / batch_size) ** .5)
     model = get_training_model(
         model_name=model_name,
+        optimizer=optimizer,
         iter_size=iter_size,
-        initial_lr=initial_lr,
-        weight_decay=weight_decay,
-        weight_decay_normalizer=weight_decay_normalizer)
+        weight_decay=weight_decay)
     model.fit(
         x=ds_train,
         steps_per_epoch=1281167 // batch_size,
-        validation_data=ds_validation,
+        validation_data=ds_valid,
         validation_steps=50000 // batch_size,
         callbacks=[lrate, model_ckpt, tensorboard],
-        # The following doesn't seem to help.
+        # The following doesn't seem to help in terms of speed.
         # use_multiprocessing=True, workers=4,
         epochs=epochs)
 
@@ -103,6 +106,8 @@ def main():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('--dataset_dir', type=str,
                         default=config.DEFAULT_DATASET_DIR)
+    parser.add_argument('--optimizer', type=str, default='adam',
+                        choices=['sgd', 'adam'])
     parser.add_argument('--batch_size', type=int, default=-1)
     parser.add_argument('--iter_size', type=int, default=-1)
     parser.add_argument('--initial_lr', type=float, default=-1.)
@@ -116,7 +121,7 @@ def main():
     os.makedirs(config.SAVE_DIR, exist_ok=True)
     os.makedirs(config.LOG_DIR, exist_ok=True)
     config_keras_backend()
-    train(args.model, args.batch_size, args.iter_size,
+    train(args.model, args.optimizer, args.batch_size, args.iter_size,
           args.initial_lr, args.final_lr, args.weight_decay,
           args.epochs, args.dataset_dir)
 

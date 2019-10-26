0955c3cfc0ce205a465a4b4daaa03b720655ad67
JK Jung <jkjung13@gmail.com>
2019-10-26 20:34:35 +0800

    Try learning rates 1e-3/1e-4/1e-5 for the resnet0 model

--- train_resnet50.sh
+++ train_resnet50.sh
@@ -3,14 +3,14 @@
 set -xe
 
 # Train resnet50 ILSVRC2012 model in 3 stages.
-# Learning rates for the 3 stages: 3e-3, 3e-4, 3e-5
-# Total 30 + 30 + 30 = 90 epochs.
+# Learning rates for the 3 stages: 1e-3, 1e-4, 1e-5
+# Total 20 + 20 + 20 = 60 epochs.
 
-python3 train.py --optimizer adam --initial_lr 3e-3 --final_lr 3e-3 --weight_decay 0.0 --epochs 30 resnet50
+python3 train.py --optimizer adam --initial_lr 1e-3 --final_lr 1e-3 --weight_decay 0.0 --epochs 20 resnet50
 cp saves/resnet50-model-final.h5 saves/resnet50-stage1.h5
 
-python3 train.py --optimizer adam --initial_lr 3e-4 --final_lr 3e-4 --weight_decay 0.0 --epochs 30 saves/resnet50-stage1.h5
+python3 train.py --optimizer adam --initial_lr 1e-4 --final_lr 1e-4 --weight_decay 0.0 --epochs 20 saves/resnet50-stage1.h5
 cp saves/resnet50-model-final.h5 saves/resnet50-stage2.h5
 
-python3 train.py --optimizer adam --initial_lr 3e-5 --final_lr 3e-5 --weight_decay 0.0 --epochs 30 saves/resnet50-stage2.h5
+python3 train.py --optimizer adam --initial_lr 1e-5 --final_lr 1e-5 --weight_decay 0.0 --epochs 20 saves/resnet50-stage2.h5
 cp saves/resnet50-model-final.h5 saves/resnet50-stage3.h5

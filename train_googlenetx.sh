de265ec92c4b2d110cb491f300269083df87e70d
JK Jung <jkjung13@gmail.com>
2019-11-16 21:25:19 +0800

    Adjust learning rate schedule and total epochs

--- train_googlenetx.sh
+++ train_googlenetx.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-# Train googlenetx ILSVRC2012 model in 120 epochs.
+# Train googlenetx ILSVRC2012 model.
 
-python3 train.py --optimizer adam --batch_size 32 --iter_size 1 --lr_sched linear --initial_lr 1e-2 --final_lr 1e-4 --weight_decay 1e-4 --epochs 120 googlenetx
+python3 train.py --optimizer adam --batch_size 64 --iter_size 1 --lr_sched linear --initial_lr 1e-2 --final_lr 1e-4 --weight_decay 1e-4 --epochs 40 googlenetx

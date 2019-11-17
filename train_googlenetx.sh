ac07da0852abbc2b2f7a46d55262a6426e316502
JK Jung <jkjung13@gmail.com>
2019-11-18 07:58:11 +0800

    Use exponentially decaying learning rates

--- train_googlenetx.sh
+++ train_googlenetx.sh
@@ -4,4 +4,4 @@ set -xe
 
 # Train googlenetx ILSVRC2012 model.
 
-python3 train.py --optimizer adam --batch_size 64 --iter_size 1 --lr_sched linear --initial_lr 1e-2 --final_lr 1e-4 --weight_decay 1e-4 --epochs 40 googlenetx
+python3 train.py --optimizer adam --batch_size 64 --iter_size 1 --lr_sched exp --initial_lr 1e-2 --lr_decay 0.8413951416451951 --weight_decay 1e-4 --epochs 40 googlenetx

d5d0f142e0136a494f457f9032a2ad2d8a706022
JK Jung <jkjung13@gmail.com>
2019-11-30 01:14:47 +0800

    Add stage2 (SGD) training

--- train_googlenet_bn.sh
+++ train_googlenet_bn.sh
@@ -3,5 +3,13 @@
 set -xe
 
 # Train googlenet_bn ILSVRC2012 model.
+# Total epochs: 20 + 40 = 60
 
 python3 train.py --optimizer adam --use_lookahead --batch_size 64 --iter_size 1 --lr_sched exp --initial_lr 1e-2 --lr_decay 0.7943282347242815 --weight_decay 2.5e-4 --epochs 20 googlenet_bn
+
+mv saves/googlenet_bn-model-final.h5 saves/googlenet_bn-stage1.h5
+rm saves/googlenet_bk-ckpt-*
+
+python3 train.py --optimizer sgd --use_lookahead --batch_size 64 --iter_size 1 --lr_sched linear --initial_lr 1e-4 --final_lr 1e-6 --weight_decay 2.5e-4 --epochs 40 saves/googlenet_bn-stage1.h5
+
+cp saves/googlenet_bn-model-final.h5 saves/googlenet_bn-stage2.h5

0fb89058d3b3abfad6ec40fdd5d1e737147acde1
JK Jung <jkjung13@gmail.com>
2019-12-02 20:13:59 +0800

    Use exponentially decaying lr schedule

--- train_resnet50.sh
+++ train_resnet50.sh
@@ -2,15 +2,9 @@
 
 set -xe
 
-# Train resnet50 ILSVRC2012 model in 3 stages.
-# Learning rates for the 3 stages: 1e-3, 1e-4, 1e-5
-# Total 20 + 20 + 20 = 60 epochs.
+# Train resnet50 ILSVRC2012 model in 80 epochs.
+# Learning rates: starting from 1e-2, exponentially decaying to 1e-5
 
-python3 train.py --optimizer adam --initial_lr 1e-3 --final_lr 1e-3 --weight_decay 0.0 --epochs 20 resnet50
+python3 train.py --optimizer adam --use_lookahead --batch_size 16 --iter_size 1 --lr_sched exp --initial_lr 1e-2 --lr_decay 0.9172759353897796 --weight_decay 1e-4 --epochs 80 resnet50
+
 cp saves/resnet50-model-final.h5 saves/resnet50-stage1.h5
-
-python3 train.py --optimizer adam --initial_lr 1e-4 --final_lr 1e-4 --weight_decay 0.0 --epochs 20 saves/resnet50-stage1.h5
-cp saves/resnet50-model-final.h5 saves/resnet50-stage2.h5
-
-python3 train.py --optimizer adam --initial_lr 1e-5 --final_lr 1e-5 --weight_decay 0.0 --epochs 20 saves/resnet50-stage2.h5
-cp saves/resnet50-model-final.h5 saves/resnet50-stage3.h5

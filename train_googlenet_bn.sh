21f25c919f4c9e653bb69bae10d790b2cc80800b
JK Jung <jkjung13@gmail.com>
2019-12-23 23:44:05 +0800

    Use exp decay learning rates

--- train_googlenet_bn.sh
+++ train_googlenet_bn.sh
@@ -2,14 +2,14 @@
 
 set -xe
 
-# Train googlenet_bn ILSVRC2012 model.
-# Total epochs: 20 + 40 = 60
+# Train googlenet_bn ILSVRC2012 model in 60 epochs.
+# Learning rates: exponential decay from 1e-2 to 1e-6
 
-python3 train.py --optimizer adam --use_lookahead --batch_size 64 --iter_size 1 --lr_sched exp --initial_lr 1e-2 --lr_decay 0.7943282347242815 --weight_decay 2.5e-4 --epochs 20 googlenet_bn
+python3 train.py --optimizer adam \
+	         --batch_size 64 --iter_size 1 \
+		 --lr_sched exp --initial_lr 1e-2 \
+		 --lr_decay 0.8576958985908941 \
+		 --weight_decay 1e-4 --epochs 60 \
+		 googlenet_bn
 
-mv saves/googlenet_bn-model-final.h5 saves/googlenet_bn-stage1.h5
-rm saves/googlenet_bk-ckpt-*
-
-python3 train.py --optimizer sgd --use_lookahead --batch_size 64 --iter_size 1 --lr_sched linear --initial_lr 1e-4 --final_lr 1e-6 --weight_decay 2.5e-4 --epochs 40 saves/googlenet_bn-stage1.h5
-
-cp saves/googlenet_bn-model-final.h5 saves/googlenet_bn-stage2.h5
+cp saves/googlenet_bn-model-final.h5 saves/googlenet_bn-stage1.h5

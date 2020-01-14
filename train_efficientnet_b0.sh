a54fa72be329281aef593d3b2073f421fd221b6e
JK Jung <jkjung13@gmail.com>
2020-01-14 10:09:58 +0800

    Add a dropout layer in efficientnet_b0

--- train_efficientnet_b0.sh
+++ train_efficientnet_b0.sh
@@ -3,12 +3,12 @@
 set -xe
 
 # Train efficientnet_b0 ILSVRC2012 model in 60 epochs.
-# Learning rates: exponential decay from 1e-2 to 1e-6
+# Learning rates: exponential decay from 1e-2 to 3e-5
 
-python3 train.py --optimizer adam \
+python3 train.py --dropout_rate 0.4 --optimizer adam \
                  --batch_size 64 --iter_size 1 \
                  --lr_sched exp --initial_lr 1e-2 \
-                 --lr_decay 0.8576958985908941 \
+                 --lr_decay 0.9077202433980235 \
                  --weight_decay 1e-4 --epochs 60 \
                  efficientnet_b0
 

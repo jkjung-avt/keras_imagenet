837f6c7878f24ab7dc50b532f83434277a29bd64
JK Jung <jkjung13@gmail.com>
2020-02-03 08:37:43 +0800

    Set dropout rate to 0.2

--- train_efficientnet_b0.sh
+++ train_efficientnet_b0.sh
@@ -5,7 +5,7 @@ set -xe
 # Train efficientnet_b0 ILSVRC2012 model in 60 epochs.
 # Learning rates: exponential decay from 1e-2 to 3e-5
 
-python3 train.py --dropout_rate 0.4 --optimizer adam \
+python3 train.py --dropout_rate 0.2 --optimizer adam \
                  --batch_size 64 --iter_size 1 \
                  --lr_sched exp --initial_lr 1e-2 \
                  --lr_decay 0.9077202433980235 \

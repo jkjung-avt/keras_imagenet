c740f332f229254a3fa54491c2f11ccf2ba04d19
JK Jung <jkjung13@gmail.com>
2020-03-09 20:01:16 +0800

    Adjust learning rates

--- train_new.sh
+++ train_new.sh
@@ -20,62 +20,62 @@ fi
 
 case $1 in
     mobilenet_v2 )
-        # Learning rates: exponential decay from 1e-2 to 1e-4
+        # Learning rates exp decay from 1e-2 to 1e-4, 60 epochs
         python3 train.py --dropout_rate 0.2 \
                          --optimizer adam --batch_size 32 --iter_size 1 \
                          --lr_sched exp --initial_lr 1e-2 \
-                         --lr_decay 0.9261187281287935 \
-                         --weight_decay 2e-4 --epochs 60 mobilenet_v2
+                         --lr_decay 0.9249147277217333 \
+                         --weight_decay 4e-5 --epochs 60 mobilenet_v2
         ;;
     resnet50 )
-        # Learning rates: exponential decay from 1e-2 to 1e-5
+        # Learning rates exp decay from 1e-2 to 1e-5, 80 epochs
         python3 train.py --dropout_rate 0.5 \
                          --optimizer adam --batch_size 16 --iter_size 1 \
                          --lr_sched exp --initial_lr 1e-2 \
-                         --lr_decay 0.8912509381337456 \
+                         --lr_decay 0.9162739011886736 \
                          --weight_decay 2e-4 --epochs 80 resnet50
         ;;
     googlenet_bn )
-        # Learning rates: exponential decay from 1e-2 to 1e-5
+        # Learning rates exp decay from 1e-2 to 1e-5, 60 epochs
         python3 train.py --dropout_rate 0.4 \
                          --optimizer adam --batch_size 32 --iter_size 1 \
                          --lr_sched exp --initial_lr 1e-2 \
-                         --lr_decay 0.8912509381337456 \
+                         --lr_decay 0.8895134973108234 \
                          --weight_decay 2e-4 --epochs 60 googlenet_bn
         ;;
     inception_v2 )
-        # Learning rates: exponential decay from 1e-2 to 1e-5
+        # Learning rates exp decay from 1e-2 to 1e-5, 60 epochs
         python3 train.py --dropout_rate 0.4 \
                          --optimizer adam --batch_size 32 --iter_size 1 \
                          --lr_sched exp --initial_lr 1e-2 \
-                         --lr_decay 0.8912509381337456 \
+                         --lr_decay 0.8895134973108234 \
                          --weight_decay 2e-4 --epochs 60 inception_v2
         ;;
     efficientnet_b0 )
-        # Learning rates: exponential decay from 1e-2 to 3e-5
+        # Learning rates exp decay from 1e-2 to 1e-4, 60 epochs
         python3 train.py --dropout_rate 0.2 \
-                         --optimizer adam --batch_size 64 --iter_size 1 \
+                         --optimizer adam --batch_size 32 --iter_size 1 \
                          --lr_sched exp --initial_lr 1e-2 \
-                         --lr_decay 0.9077202433980235 \
+                         --lr_decay 0.9249147277217333 \
                          --weight_decay 1e-4 --epochs 60 \
                          efficientnet_b0
         ;;
     efficientnet_b1 )
-        # Learning rates: exponential decay from 1e-2 to 3e-5
+        # Learning rates exp decay from 1e-2 to 1e-4, 60 epochs
         python3 train.py --dropout_rate 0.2 \
-                         --optimizer adam --batch_size 64 --iter_size 1 \
+                         --optimizer adam --batch_size 32 --iter_size 1 \
                          --lr_sched exp --initial_lr 1e-2 \
-                         --lr_decay 0.9077202433980235 \
+                         --lr_decay 0.9249147277217333 \
                          --weight_decay 1e-4 --epochs 60 \
                          efficientnet_b1
         ;;
     efficientnet_b4 )
-        # Learning rates: exponential decay from 1e-2 to 3e-5
+        # Learning rates exp decay from 1e-2 to 1e-4, 80 epochs
         python3 train.py --dropout_rate 0.2 \
-                         --optimizer adam --batch_size 64 --iter_size 1 \
+                         --optimizer adam --batch_size 16 --iter_size 1 \
                          --lr_sched exp --initial_lr 1e-2 \
-                         --lr_decay 0.9077202433980235 \
-                         --weight_decay 1e-4 --epochs 60 \
+                         --lr_decay 0.9433732216299777 \
+                         --weight_decay 1e-4 --epochs 80 \
                          efficientnet_b4
         ;;
     * )
